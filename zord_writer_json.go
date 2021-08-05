f78c523c595d6fbba316d2b0e129db2c8e890b9f
Don Tong <don@7fffffff.com>
2021-08-05 00:32:32 -0700

    recover if there's a panic during reordering

--- zord_writer_json.go
+++ zord_writer_json.go
@@ -2,9 +2,13 @@
 
 package zord
 
+import (
+	"fmt"
+)
+
 func (z ZordWriter) Write(event []byte) (n int, err error) {
 	obj := make([]byte, 0, len(event))
-	obj, n, err = reorder(obj, event, z.FirstKeys)
+	obj, n, err = tryReorder(obj, event, z.FirstKeys)
 	if err != nil {
 		// If there's an error in the reordering process, it's more
 		// important that the log data get written. So write the event
@@ -17,4 +21,17 @@ func (z ZordWriter) Write(event []byte) (n int, err error) {
 	}
 	_, err = z.Output.Write([]byte("\n"))
 	return n, err
+}
+
+func tryReorder(dest, src []byte, firstKeys []string) (extended []byte, n int, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			if recoveredErr, ok := r.(error); ok {
+				err = recoveredErr
+			} else {
+				err = fmt.Errorf("panic: %v", r)
+			}
+		}
+	}()
+	return reorder(dest, src, firstKeys)
 }
